12e0a120bf0f7c4a01230a07eeb0836c3899d379
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-01-17 22:58:08 +0900

    [Bronze III] Title: 최댓값, Time: 132 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,29 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-
-		int nums[][] = new int[10][10];
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[][] intList = new int[9][9];
 		int max = 0;
-		int x = 0, y = 0;
-
-		for (int i = 0; i < 9; i++) 
-		{
-			for (int j = 0; j < 9; j++) 
-			{
-				nums[i][j] = sc.nextInt();
-				if (max < nums[i][j]) 
-				{
-					max = nums[i][j];
-					x = i; y = j;
+		int maxI = 0, maxJ = 0;
+		
+		for(int i=0; i<9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0; j<9; j++) {
+				intList[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				if (max < intList[i][j]) {
+					max = intList[i][j];
+					maxI = i; maxJ = j;
 				}
 			}
 		}
-
 		System.out.println(max);
-		System.out.println((x + 1) + " " + (y + 1));
-	}
+		System.out.printf("%d %d",maxI+1,maxJ+1);
+	}	
 }
