efd5a919efbe251b4c0ff55260cbcadf5022e5e0
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-01-25 21:46:01 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int space = 0;
+		for(int i=0; i<numlist.length; i++) {
+			for(int j=0; j<numlist.length; j++) {
+				if(Math.abs(n-numlist[i]) < Math.abs(n-numlist[j])) {
+					space = numlist[i];
+					numlist[i] = numlist[j];
+					numlist[j] = space;
+				} else if (Math.abs(n-numlist[i]) == Math.abs(n-numlist[j]) && numlist[i]>numlist[j]){
+					space = numlist[i];
+					numlist[i] = numlist[j];
+					numlist[j] = space;
+				}
+			}
+		}
+        return numlist;
+    }
+}
