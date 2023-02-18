dbf68794e6883da39c0988bbed709bce10abfcf0
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-02-18 10:10:45 +0900

    [Gold IV] Title: N-Queen, Time: 6884 ms, Memory: 18540 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -13,21 +13,26 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		// 사이즈 N과 chess 위치를 표시할 리스트를 만듦
 		N = Integer.parseInt(br.readLine());
-		
 		chess = new ArrayList<>();
 		
+		// 백트래킹 시작
 		backTracking(0);
 		
+		// 체스 둘 수 있는 개수를 출력
 		System.out.println(cnt);
 	}
 
 	private static void backTracking(int k) {
+		// N개가 모두 배치되면 끝!!
 		if(k==N) {
 			cnt++;
 			return;
 		}
 		
+		// chess의 idx가 열, check의 idx의 value가 행!!
+		// 이전에 찍었던 값들과 비교해서 둘 수 있는 자리이면 둔다.
 		for(int i=0; i<N; i++) {
 			if(check(k, i)) {
 				chess.add(i);
@@ -41,10 +46,12 @@ public class Main {
 	}
 	private static boolean check(int x, int y) {
 		for(int i=0; i<chess.size(); i++) {
+			// 행, 열 일치 시 못두고, 대각선에도 못둠
 			if(i == x || y == chess.get(i) || i + chess.get(i) == x + y || i - x == chess.get(i) - y) {
 				return false;
 			}
 		}
+		// 일치하지 않으면 true 반환
 		return true;
 	}
 }
