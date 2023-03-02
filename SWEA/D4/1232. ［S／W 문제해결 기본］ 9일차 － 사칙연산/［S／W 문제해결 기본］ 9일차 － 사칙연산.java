680028e91dd4ca38d4a261591fc8a80dce1e6884
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-03-02 12:28:18 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 185 ms, Memory: 35,328 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
@@ -1,165 +1,181 @@
 import java.util.Scanner;
 
 public class Solution {
-    private static String[] input;
-    public static void main(String[] args) {
-        // 숫자를 입력받고 String 배열을 만든다.
-        Scanner sc = new Scanner(System.in);
 
-        for(int t=1; t<=10; t++) {
+    private static String[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        for (int t = 1; t <= 10; t++) {
+
             int N = Integer.parseInt(sc.nextLine());
 
-            input = new String[N + 1];
+            // N+1 크기의 배열을 만든 후 인덱스에 맞게 값을 넣어준다.
+            arr = new String[N + 1];
 
-            // String을 입력받으면 차례로 1 ~ size 배열에 넣는다.
+            // 문자열 형태로 배열에 넣는다
             for (int i = 1; i <= N; i++) {
-                input[i] = sc.nextLine();
+                arr[i] = sc.nextLine();
             }
 
-            // 트리 형태이므로 맨 위의 값인 첫 번째 String부터 시작하면 된다.
-            // 꺼낸 String의 1번째 값이 연산자이면 해당 연산 method 호출 / 숫자이면 계산
+            // 1번부터 진행한다.
+            String[] calculate = arr[1].split(" ");
+
+            // answer을 0으로 두고, 결과를 반환받는다.
             int answer = 0;
 
-            if (input[1].split(" ")[1].equals("+")) {
-                answer = addFunc(Integer.parseInt(input[1].split(" ")[2]), Integer.parseInt(input[1].split(" ")[3]));
-            } else if (input[1].split(" ")[1].equals("-")) {
-                answer = subFunc(Integer.parseInt(input[1].split(" ")[2]), Integer.parseInt(input[1].split(" ")[3]));
-            } else if (input[1].split(" ")[1].equals("*")) {
-                answer = multiFunc(Integer.parseInt(input[1].split(" ")[2]), Integer.parseInt(input[1].split(" ")[3]));
-            } else if (input[1].split(" ")[1].equals("/")) {
-                answer = divFunc(Integer.parseInt(input[1].split(" ")[2]), Integer.parseInt(input[1].split(" ")[3]));
+            // 1번 idx의 값이 연산자이면 연산자 메서드 호출
+            if (calculate[1].equals("-")) {
+                answer = minus(calculate[2], calculate[3]);
+            } else if (calculate[1].equals("+")) {
+                answer = plus(calculate[2], calculate[3]);
+            } else if (calculate[1].equals("*")) {
+                answer = multi(calculate[2], calculate[3]);
+            } else if (calculate[1].equals("/")) {
+                answer = div(calculate[2], calculate[3]);
+            } else {
+                answer = Integer.parseInt(calculate[1]);
             }
-            System.out.printf("#%d %d\n", t, answer);
+
+            // 결과를 출력.
+            sb.append(String.format("#%d %d\n", t, answer));
         }
+        System.out.println(sb);
     }
 
-    // 아래의 다른 function은 리턴값만 다르기 때문에 하나만 설명하자면,
-    private static int addFunc(int a, int b) {
-        // 계산을 수행할 값을 넣을 A와 B를 선언한다.
-        int A = 0;
-        int B = 0;
+    private static int minus(String a, String b) {
+        // a와 b를 받아서 해당 값의 1번째 idx 값이 연산자이면 또 연산자를 호출하고,
+        // 둘 다 정수이면 계산한 결과를 반환한다.
+        String[] A = arr[Integer.parseInt(a)].split(" ");
+        String[] B = arr[Integer.parseInt(b)].split(" ");
 
-        // 정수 a, b를 받아서 input에 넣으면 해당 idx의 String이 호출된다.
-        // 이 때 String을 split한 값의 두 번째는 연산자이거나, 숫자이다.
-        // 연산자일 경우 세 번째 값과 네 번째 값은 정수이므로 새로운 함수를 호출한다.
-        // 숫자일 경우 A 또는 B에 저장하여 계산한다.
-        if(input[a].split(" ")[1].equals("+")){
-            A = addFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("-")){
-            A = subFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("*")){
-            A = multiFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("/")) {
-            A = divFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
+        int resultA = 0;
+        int resultB = 0;
+        if(A[1].equals("-")) {
+            resultA = minus(A[2], A[3]);
+        } else if (A[1].equals("+")) {
+            resultA = plus(A[2], A[3]);
+        } else if (A[1].equals("*")) {
+            resultA = multi(A[2], A[3]);
+        } else if (A[1].equals("/")) {
+            resultA = div(A[2], A[3]);
         } else {
-            A = Integer.parseInt(input[a].split(" ")[1]);
+            resultA = Integer.parseInt(A[1]);
         }
 
-        if(input[b].split(" ")[1].equals("+")){
-            B = addFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("-")){
-            B = subFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("*")){
-            B = multiFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("/")) {
-            B = divFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
+        if(B[1].equals("-")) {
+            resultB = minus(B[2], B[3]);
+        } else if (B[1].equals("+")) {
+            resultB = plus(B[2], B[3]);
+        } else if (B[1].equals("*")) {
+            resultB = multi(B[2], B[3]);
+        } else if (B[1].equals("/")) {
+            resultB = div(B[2], B[3]);
         } else {
-            B = Integer.parseInt(input[b].split(" ")[1]);
+            resultB = Integer.parseInt(B[1]);
         }
 
-        return A + B;
+        return resultA - resultB;
     }
 
-    private static int subFunc(int a, int b) {
-        int A = 0;
-        int B = 0;
+    private static int plus(String a, String b) {
+        String[] A = arr[Integer.parseInt(a)].split(" ");
+        String[] B = arr[Integer.parseInt(b)].split(" ");
 
-        if(input[a].split(" ")[1].equals("+")){
-            A = addFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("-")){
-            A = subFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("*")){
-            A = multiFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("/")) {
-            A = divFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
+        int resultA = 0;
+        int resultB = 0;
+        if(A[1].equals("-")) {
+            resultA = minus(A[2], A[3]);
+        } else if (A[1].equals("+")) {
+            resultA = plus(A[2], A[3]);
+        } else if (A[1].equals("*")) {
+            resultA = multi(A[2], A[3]);
+        } else if (A[1].equals("/")) {
+            resultA = div(A[2], A[3]);
         } else {
-            A = Integer.parseInt(input[a].split(" ")[1]);
+            resultA = Integer.parseInt(A[1]);
         }
 
-        if(input[b].split(" ")[1].equals("+")){
-            B = addFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("-")){
-            B = subFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("*")){
-            B = multiFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("/")) {
-            B = divFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
+        if(B[1].equals("-")) {
+            resultB = minus(B[2], B[3]);
+        } else if (B[1].equals("+")) {
+            resultB = plus(B[2], B[3]);
+        } else if (B[1].equals("*")) {
+            resultB = multi(B[2], B[3]);
+        } else if (B[1].equals("/")) {
+            resultB = div(B[2], B[3]);
         } else {
-            B = Integer.parseInt(input[b].split(" ")[1]);
+            resultB = Integer.parseInt(B[1]);
         }
 
-        return Math.abs(A - B);
+        return resultA + resultB;
     }
 
-    private static int multiFunc(int a, int b) {
-        int A = 0;
-        int B = 0;
+    private static int div(String a, String b) {
+        String[] A = arr[Integer.parseInt(a)].split(" ");
+        String[] B = arr[Integer.parseInt(b)].split(" ");
 
-        if(input[a].split(" ")[1].equals("+")){
-            A = addFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("-")){
-            A = subFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("*")){
-            A = multiFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("/")) {
-            A = divFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
+        int resultA = 0;
+        int resultB = 0;
+        if(A[1].equals("-")) {
+            resultA = minus(A[2], A[3]);
+        } else if (A[1].equals("+")) {
+            resultA = plus(A[2], A[3]);
+        } else if (A[1].equals("*")) {
+            resultA = multi(A[2], A[3]);
+        } else if (A[1].equals("/")) {
+            resultA = div(A[2], A[3]);
         } else {
-            A = Integer.parseInt(input[a].split(" ")[1]);
+            resultA = Integer.parseInt(A[1]);
         }
 
-        if(input[b].split(" ")[1].equals("+")){
-            B = addFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("-")){
-            B = subFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("*")){
-            B = multiFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("/")) {
-            B = divFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
+        if(B[1].equals("-")) {
+            resultB = minus(B[2], B[3]);
+        } else if (B[1].equals("+")) {
+            resultB = plus(B[2], B[3]);
+        } else if (B[1].equals("*")) {
+            resultB = multi(B[2], B[3]);
+        } else if (B[1].equals("/")) {
+            resultB = div(B[2], B[3]);
         } else {
-            B = Integer.parseInt(input[b].split(" ")[1]);
+            resultB = Integer.parseInt(B[1]);
         }
 
-        return A * B;
+        return resultA / resultB;
     }
 
-    private static int divFunc(int a, int b) {
-        int A = 0;
-        int B = 0;
+    private static int multi(String a, String b) {
+        String[] A = arr[Integer.parseInt(a)].split(" ");
+        String[] B = arr[Integer.parseInt(b)].split(" ");
 
-        if(input[a].split(" ")[1].equals("+")){
-            A = addFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("-")){
-            A = subFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("*")){
-            A = multiFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
-        } else if(input[a].split(" ")[1].equals("/")) {
-            A = divFunc(Integer.parseInt(input[a].split(" ")[2]), Integer.parseInt(input[a].split(" ")[3]));
+        int resultA = 0;
+        int resultB = 0;
+        if(A[1].equals("-")) {
+            resultA = minus(A[2], A[3]);
+        } else if (A[1].equals("+")) {
+            resultA = plus(A[2], A[3]);
+        } else if (A[1].equals("*")) {
+            resultA = multi(A[2], A[3]);
+        } else if (A[1].equals("/")) {
+            resultA = div(A[2], A[3]);
         } else {
-            A = Integer.parseInt(input[a].split(" ")[1]);
+            resultA = Integer.parseInt(A[1]);
         }
 
-        if(input[b].split(" ")[1].equals("+")){
-            B = addFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("-")){
-            B = subFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("*")){
-            B = multiFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
-        } else if(input[b].split(" ")[1].equals("/")) {
-            B = divFunc(Integer.parseInt(input[b].split(" ")[2]), Integer.parseInt(input[b].split(" ")[3]));
+        if(B[1].equals("-")) {
+            resultB = minus(B[2], B[3]);
+        } else if (B[1].equals("+")) {
+            resultB = plus(B[2], B[3]);
+        } else if (B[1].equals("*")) {
+            resultB = multi(B[2], B[3]);
+        } else if (B[1].equals("/")) {
+            resultB = div(B[2], B[3]);
         } else {
-            B = Integer.parseInt(input[b].split(" ")[1]);
+            resultB = Integer.parseInt(B[1]);
         }
-        if(A >= B) return A / B;
-        else return B / A;
+
+        return resultA * resultB;
     }
 }
