a5d4946f9f1882aae1bf4168495e683380f68fbc
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-03-21 21:09:39 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 116 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -11,7 +11,10 @@ public class Main {
 
         int testCase = Integer.parseInt(br.readLine());
 
+        // testCase만큼 반복 진행
         for (int tc = 0; tc < testCase; tc++) {
+
+            // 
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int x = Integer.parseInt(st.nextToken());
@@ -19,8 +22,11 @@ public class Main {
 
             long distance = y - x;
 
+            // 규칙 상 n제곱의 위치에 따라 홀수와 짝수가 정해지기에, 시작점을 루트 distance로 잡고 시작한다.
             long n = (long) Math.sqrt(distance);
 
+            // n2-n < distance <= n2이면 홀수
+            // n2 < distance <= n2+n이면 짝수가 나온다.
             while (true) {
                 if (distance > n * n - n && distance <= n * n) {
                     sb.append(2 * n - 1).append("\n");
@@ -32,6 +38,7 @@ public class Main {
                 n++;
             }
         }
+        // 결과 출력
         System.out.println(sb);
     }
 }
