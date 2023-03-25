6c1f070906492a6ba4434dfe8ae203d0ff9c8a7f
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-03-25 15:41:15 +0900

    Update N과 M （11）.java

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -14,34 +14,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        int[] input = new int[N];
-
+        
         st = new StringTokenizer(br.readLine());
 
+        // set에 입력받기(중복되는 수 알아서 제거)
         HashSet<Integer> set = new HashSet<>();
 
+        // N개의 숫자를 set에 입력받는다.
         for (int i = 0; i < N; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
 
+        // N을 set의 크기로 바꿔주고
         N = set.size();
 
+        // arr를 만들어준다.
         arr = new int[N];
+        int[] answer = new int[M];
 
+        // set에 있는 숫자를 꺼내서 arr에 넣어준다.
         int idx = 0;
         for(int a : set) {
             arr[idx++] = a;
         }
-
+        // 정렬해준 뒤 백트래킹 시작
         Arrays.sort(arr);
-
-        sb = new StringBuilder();
-
-        int[] answer = new int[M];
 
         backTracking(answer, 0);
 
@@ -49,6 +50,7 @@ public class Main {
     }
 
     private static void backTracking(int[] answer, int k) {
+        // k가 M에 도달하면 answer에 있는 원소 출력하기
         if(k == M) {
             for (int i = 0; i < M; i++) {
                 sb.append(answer[i]).append(" ");
@@ -57,6 +59,7 @@ public class Main {
             return;
         }
 
+        // arr에 있는 원소에 대해서 모든 경우의 수 골라주기
         for (int i = 0; i < N; i++) {
             answer[k] = arr[i];
             backTracking(answer, k+1);
