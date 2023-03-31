11721d04190cec4ae3dd8c21a8ce7b31c0092097
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-03-31 20:29:25 +0900

    [Silver II] Title: 좌표 압축, Time: 1872 ms, Memory: 266276 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int idx = 0;
+        for (int i = 0; i < N; i++) {
+            if (map.get(tmp[i]) == null) {
+                map.put(tmp[i], idx++);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
