ed37d001ab170ba4e6441bcffac6ca7047e2278d
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-04-02 09:36:31 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1600 ms, Memory: 148544 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
         List<Integer> list = new ArrayList<>();
 
-        st = new StringTokenizer(sc.nextLine());
+        st = new StringTokenizer(br.readLine());
         int max = 0;
         for (int i = 0; i < N; i++) {
             list.add(Integer.parseInt(st.nextToken()));
