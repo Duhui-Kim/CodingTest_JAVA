99dc1c6c4bd952b9479f5b86da63ece1c850982c
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-04-09 09:54:22 +0900

    [Gold V] Title: 동전 2, Time: 132 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -12,10 +12,15 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        // 동전의 개수를 체크할 dp 배열 선언
         int[] dp = new int[k+1];
+        
+        // 큰 값으로 채워넣고 초기값 0 저장
         Arrays.fill(dp, Integer.MAX_VALUE / 2);
         dp[0] = 0;
 
+        // coin을 입력받고, i원에서 i+coin 값이 되기 위한 개수는 i일 때의 개수 + 1
+        // 그 중에서 최솟값을 dp 배열에 저장한다.
         for (int i = 0; i < n; i++) {
             int coin = Integer.parseInt(br.readLine());
 
@@ -24,6 +29,8 @@ public class Main {
             }
         }
 
+        // k원의 값이 초기값과 같으면 불가능한 경우이므로 -1 출력
+        // 아닌 경우 값 출력
         if (dp[k] == Integer.MAX_VALUE / 2) {
             System.out.println(-1);
         } else {
