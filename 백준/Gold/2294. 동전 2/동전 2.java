95d9dc2bd2640deb07f617f0ff33853bad44cc9a
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-04-09 09:52:38 +0900

    [Gold V] Title: 동전 2, Time: 140 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[k+1];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            int coin = Integer.parseInt(br.readLine());
+
+            for (int j = coin; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j-coin] + 1);
+            }
+        }
+
+        if (dp[k] == Integer.MAX_VALUE / 2) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[k]);
+        }
+    }
+}
