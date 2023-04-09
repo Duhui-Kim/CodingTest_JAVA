bdf5d8ebf4c17dbc5e8da7c6664695a1be4870d5
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-04-09 22:56:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 756 ms, Memory: 59936 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static class Pair implements Comparable<Pair> {
+        int x;
+        int y;
+        private Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        @Override
+        public String toString() {
+            return x + " " + y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            if (x == o.x) return Integer.compare(y, o.y);
+            return Integer.compare(x, o.x);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        ArrayList<Pair> list = new ArrayList<>(N);
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            list.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++) {
+            sb.append(list.get(i).toString()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
