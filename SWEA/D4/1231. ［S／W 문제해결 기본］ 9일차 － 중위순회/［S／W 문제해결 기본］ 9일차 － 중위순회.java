92aca57e5432b52c453de16bbe47fdec40accbc1
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-06-12 15:57:27 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 136 ms, Memory: 21,296 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -1,44 +1,59 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
 
-        for (int t = 1; t <= 10; t++) {
+	private static class Node {
+		String word;
+		int left, right;
+		
+		public Node(String word, int left, int right) {
+			this.word = word;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	private static Node[] nodes;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=10; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			nodes = new Node[N+1];
+			
+			for(int i=0; i<N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int idx = Integer.parseInt(st.nextToken());
+				String word = st.nextToken();
+				
+				int left = 0;
+				int right = 0;
+				
+				if(st.hasMoreTokens()) left = Integer.parseInt(st.nextToken());
+				if(st.hasMoreTokens()) right = Integer.parseInt(st.nextToken());
+							
+				nodes[idx] = new Node(word, left, right);
+			}
+			
+			sb.append("#" + tc + " ");
+			print(sb, 1);
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
 
-            int N = Integer.parseInt(sc.nextLine());
-
-            // arr 배열의 각 칸에
-            // [자기숫자, Value, Left, Right]를 넣는다. 자기숫자 = index여서 사실 필요없긴하다.
-            String[][] arr = new String[N + 1][4];
-
-            // 본인 숫자와 idx를 맞추기 위해 1부터 N까지 idx에 맞게 값을 넣어줬다.
-            for (int i = 1; i <= N; i++) {
-                StringTokenizer st = new StringTokenizer(sc.nextLine());
-                int idx = 0;
-                while (st.hasMoreTokens()) {
-                    arr[i][idx++] = st.nextToken();
-                }
-            }
-            sb.append("#" + t + " ");
-            // 중위순회 시작(1번 idx부터)
-            traverse(arr, 1, N, sb);
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    private static void traverse(String[][] arr, int i, int N, StringBuilder sb) {
-        // i가 N 이하일 때만 진행
-        if(i <= N) {
-            // Left나 Right가 없으면 진행하지 않고,
-            // L - V - R 순으로 순회 진행
-            if(arr[i][2] != null) traverse(arr, Integer.parseInt(arr[i][2]), N, sb);
-            sb.append(arr[i][1] + "");
-            if(arr[i][3] != null) traverse(arr, Integer.parseInt(arr[i][3]), N, sb);
-        }
-    }
+	private static void print(StringBuilder sb, int idx) {
+		if(idx == 0) return;
+		
+		print(sb, nodes[idx].left);
+		sb.append(nodes[idx].word);
+		print(sb, nodes[idx].right);
+	}
 }
