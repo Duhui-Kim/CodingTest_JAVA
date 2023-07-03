5e3dc028de079c0b5690cee3c693dab95ebf663b
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-07-03 21:11:31 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1268 ms, Memory: 332200 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -7,71 +7,62 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		if(N == 2) {
+			System.out.println(0);
+			return;
+		}
+		
+		int[] parent = new int[N+1];
+		Arrays.fill(parent, -1);
+		
+		PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+				
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			queue.offer(new int[] {a, b, c});
+		}
+		
+		int cnt = 0;
+		int cost = 0;
+		while(cnt < N-2) {
+			int[] cur = queue.poll();
+			
+			if(!diffParent(cur[0], cur[1], parent)) continue;
+			
+			cost += cur[2];
+			cnt++;
+		}
+		System.out.println(cost);
+	}
 
-        // 집의 개수 N과 길의 개수 M
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+	private static boolean diffParent(int a, int b, int[] parent) {
+		a = find(a, parent);
+		b = find(b, parent);
+		
+		if(a == b) return false;
+		
+		if(parent[a] == parent[b]) parent[a]--;
+		if(parent[a] < parent[b]) parent[b] = a;
+		else parent[a] = b;
+		
+		return true;
+	}
 
-        // road 배열 생성
-        int[][] road = new int[M][3];
-
-        // 연결된 두 노드와 가중치 입력받기
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            road[i][0] = Integer.parseInt(st.nextToken());
-            road[i][1] = Integer.parseInt(st.nextToken());
-            road[i][2] = Integer.parseInt(st.nextToken());
-        }
-
-        // 가중치 순으로 정렬
-        Arrays.sort(road, (o1, o2) -> o1[2] - o2[2]);
-
-        // 부모 배열을 선언하고
-        int[] parent = new int[N+1];
-        Arrays.fill(parent, -1);
-
-        // 연결된 간선의 수를 셀 cnt 선언
-        int cnt = 0;
-
-        int answer = 0;
-
-        // 가중치 순으로 진행
-        for (int i = 0; i < M; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
-
-            // 같은 부모를 갖고 있다면 연결시켜주고 지나감
-            if(!diffParent(parent, a, b)) continue;
-
-            // 다른 부모를 갖고 있었으면 정답에 답 더해주고 cnt 올림
-            answer += road[i][2];
-            cnt++;
-            // 도시를 반으로 나눌 것이므로 N-2개의 간선이 생기면 종료
-            if (cnt == N-2) break;
-        }
-
-        System.out.println(answer);
-    }
-
-    private static boolean diffParent(int[] parent, int a, int b) {
-        a = find(parent, a);
-        b = find(parent, b);
-
-        if (a == b) return false;
-        if (parent[a] == parent[b]) parent[a]--;
-        if (parent[a] < parent[b]) parent[b] = a;
-        else parent[a] = b;
-
-        return true;
-    }
-
-    private static int find(int[] parent, int a) {
-        if(parent[a] < 0) return a;
-
-        return find(parent, parent[a]);
-    }
-
+	private static int find(int a, int[] parent) {
+		if(parent[a] < 0) return a;
+				
+		return parent[a] = find(parent[a], parent);
+	}
 }
