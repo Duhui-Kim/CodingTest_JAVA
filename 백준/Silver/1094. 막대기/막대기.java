783e5e08708240ed4b0bc77704d07840a16e2542
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2023-08-01 08:28:20 +0900

    [Silver V] Title: 막대기, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = 0; i <= 6; i++) {
+            if ((N & 1 << i) > 0) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
