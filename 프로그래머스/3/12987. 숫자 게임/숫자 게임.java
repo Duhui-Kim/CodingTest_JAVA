ecd40a4881afe2c2de1111104f8361bbb3ca0d70
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2024-04-24 19:04:17 +0900

    [level 3] Title: 숫자 게임, Time: 58.69 ms, Memory: 66.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+                
+        int answer = 0;
+        int idx = 0;
+        for (int i=0; i<B.length; i++) {
+            if (A[idx] < B[i]) {
+                answer++;
+                idx++;
+            }
+        }
+        
+        return answer;
+    }
+}
