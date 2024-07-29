28eae20097229819eebdad152eb56b1544c51405
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2024-07-29 15:41:57 +0900

    [Silver III] Title: 어린 왕자, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int tc;
+	cin >> tc;
+
+	while (tc-- > 0) {
+		int p1[2], p2[2] = { 0 };
+		int starCount;
+		int answer = 0;
+
+		cin >> p1[0] >> p1[1] >> p2[0] >> p2[1] >> starCount;
+
+		while (starCount-- > 0) {
+			int star[3] = { 0 };
+
+			cin >> star[0] >> star[1] >> star[2];
+
+			int d1 = (star[0] - p1[0]) * (star[0] - p1[0])
+				+ (star[1] - p1[1]) * (star[1] - p1[1]);
+
+			int d2 = (star[0] - p2[0]) * (star[0] - p2[0])
+				+ (star[1] - p2[1]) * (star[1] - p2[1]);
+
+			int r = star[2] * star[2];
+
+			if ((d1 < r && r < d2) || (d2 < r && r < d1)) {
+				answer++;
+			}
+		}
+		cout << answer << '\n';
+	}
+	cout << endl;
+}
