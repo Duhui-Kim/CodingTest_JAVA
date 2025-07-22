f368c3e20db356ee97eb844c169fa8ce12529590
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2025-07-22 18:32:19 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.73 ms, Memory: 3.87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, string control) {
+    for (char c : control) {
+        switch (c) {
+            case 'w':
+                n += 1;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+            case 'a':
+                n -= 10;
+                break;
+        }
+    }
+    return n;
+}
