6a526a798751cbfdeeff280768f71e412a3ca47d
Duhui-Kim <118238663+Duhui-Kim@users.noreply.github.com>
2026-01-18 21:31:36 +0900

    [level 2] Title: 더 맵게, Time: 226.80 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> q;
+    
+    for (int i=0; i<scoville.size(); i++) {
+        q.push(scoville[i]);
+    }
+        
+    while (q.top() < K) {
+        answer++;
+        int a = q.top();
+        q.pop();
+        
+        if (q.empty()) {
+            answer = -1;
+            break;
+        }
+        
+        int b = q.top();
+        q.pop();
+        
+        q.push(a + b * 2);
+    }
+      
+    return answer;
+}
